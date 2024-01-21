f485922e2f4b1d34b64ef9b7e34f629060c2af79
Abhi865625 <akmehta7252@gmail.com>
2024-01-21 11:02:31 +0530

    modified:   Day 1/index.html 	modified:   Day 1/script.js 	modified:   Day 1/style.css

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,1 +1,51 @@
-console.log("hello");
+// Array Methods:
+// forEach()
+
+/* The forEach method is commonly used for iterating over elements in an array. It is a higher-order function because it takes a callback function as its argument and applies that function to each element in the array*/
+
+const workers = [
+  {
+    name: "Abhi",
+    age: 28,
+    salary: 50000,
+    isMarried: false,
+    profession: "Developer",
+  },
+  {
+    name: "Sara",
+    age: 35,
+    salary: 60000,
+    isMarried: true,
+    profession: "Designer",
+  },
+  {
+    name: "John",
+    age: 32,
+    salary: 55000,
+    isMarried: true,
+    profession: "Engineer",
+  },
+  {
+    name: "Mike",
+    age: 40,
+    salary: 70000,
+    isMarried: false,
+    profession: "Manager",
+  },
+  {
+    name: "Lily",
+    age: 28,
+    salary: 48000,
+    isMarried: false,
+    profession: "Analyst",
+  },
+];
+
+// Here i will be finding the ' Average Salary ' of all the workers by using forEach()
+
+let totalSalary = 0;
+workers.forEach((workers) => {
+  totalSalary += workers.salary;
+});
+const avgSalary = totalSalary / workers.length;
+console.log(avgSalary);
